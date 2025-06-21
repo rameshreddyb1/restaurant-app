8cfd8186e43f69d05f1026cad76de1dd761b1264
ramesh <rameshreddyb1@gmail.com>
2025-06-21 07:13:16 +0530

    initial commit

---
+++ restaurant-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'restaurant-app';
+  isactive:boolean=false;
+  applyC1 = true;
+}
